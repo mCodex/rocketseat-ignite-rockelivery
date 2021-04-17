e1c1ea9d91a804a882536f448b97d45f13f8ed73
Mateus Andrade <mat.andrade@live.com>
2021-04-17 09:47:40 -0300

    fixed seed script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,14 +10,14 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Rockelivery.{Item,Order,Repo,User}
+alias Rockelivery.{Item, Order, Repo, User}
 
 user = %User{
   age: 27,
   address: "Street 1",
   cep: "12345678",
   cpf: "12345678901",
-  email: "user@user.com"
+  email: "user@user.com",
   name: "User"
 }
 
@@ -41,7 +41,7 @@ Repo.insert!(item1)
 
 Repo.insert!(item2)
 
-order = %Order {
+order = %Order{
   user_id: user_id,
   items: [item1, item1, item2],
   address: "Street 1",
